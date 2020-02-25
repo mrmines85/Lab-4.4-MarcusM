f5bd757ab40fc2f206d4644ca233835cb890b916
mrmines85 <61034810+mrmines85@users.noreply.github.com>
2020-02-25 17:07:42 -0600

    Add files via upload
    Lab 4.4 MarcusM

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList blackListWords = new ArrayList();
+            blackListWords.Add("buy");
+            blackListWords.Add("viagra");
+            blackListWords.Add("xxx");
+            blackListWords.Add("free money");
+            blackListWords.Add("lifetime offer");
+            blackListWords.Add("send money");
+            blackListWords.Add("bank account");
+            blackListWords.Add("nigeria");
+            blackListWords.Add("online");
+            blackListWords.Add("pharmacy");
+            blackListWords.Add("h8te");
+            blackListWords.Add("meet girls");
+
+            string message = Console.ReadLine();
+
+            foreach (string item in blackListWords)
+            {
+                bool isSpam = false;
+               
+                if (message.Contains(item))
+                {
+                    isSpam = true;
+                    Console.WriteLine("The message is spam");
+                }
+                else
+                {
+                    isSpam = false;
+                    Console.WriteLine("The message is not spam.");
+                }
+            }
+        }
+    }
+}
